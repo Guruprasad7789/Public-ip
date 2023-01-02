3d16f007f9b96ea6e79c5bb906eb49f6cd4118c6
Guruprasad7789 <57087461+Guruprasad7789@users.noreply.github.com>
2023-01-02 18:47:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.send(req.ip);
 });
 
-app.listen((process.env.PORT || 3000, () => {
-  console.log('App listening on port (process.env.PORT || 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App listening on port '+ (process.env.PORT || '3000'));
 });
